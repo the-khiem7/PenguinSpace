da0b3159891790de651c31f08fea1773db2180c8
the-khiem7 <khiemnguyen120216@gmail.com>
2026-04-01 00:33:24 +0700

    feat: refactor App startup to use Microsoft.Extensions.DependencyInjection
    - Replace manual service construction with IServiceCollection/IServiceProvider
    - Register all services as singletons, MainWindowViewModel as transient
    - Add Microsoft.Extensions.DependencyInjection 8.0.1 package to UI project

--- PenguinSpace.UI/App.axaml.cs
+++ PenguinSpace.UI/App.axaml.cs
@@ -1,6 +1,7 @@
-using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using PenguinSpace.Core.Interfaces;
 using PenguinSpace.Disk;
 using PenguinSpace.Docker;
 using PenguinSpace.Infrastructure;
@@ -9,11 +10,14 @@ using PenguinSpace.UI.Services;
 using PenguinSpace.UI.ViewModels;
 using PenguinSpace.UI.Views;
 using PenguinSpace.WSL;
+using Serilog;
 
 namespace PenguinSpace.UI;
 
 public class App : Avalonia.Application
 {
+    private IServiceProvider? _services;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -21,30 +25,41 @@ public class App : Avalonia.Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        _services = BuildServiceProvider();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            var logger = LoggingConfiguration.CreateLogger();
-            var commandRunner = new CommandRunner(logger);
-            var wslService = new WslService(commandRunner, logger);
-            var distroService = new DistroService(commandRunner, logger);
-            var dockerService = new DockerService(commandRunner, logger);
-            var diskService = new DiskService(commandRunner, logger);
-            var dialogService = new DialogService();
-
-            var viewModel = new MainWindowViewModel(
-                wslService,
-                distroService,
-                dockerService,
-                diskService,
-                dialogService);
-
+            var viewModel = _services.GetRequiredService<MainWindowViewModel>();
             var mainWindow = new MainWindow(viewModel);
             desktop.MainWindow = mainWindow;
 
-            // Load distros on startup
             _ = viewModel.LoadDistrosAsync();
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static IServiceProvider BuildServiceProvider()
+    {
+        var services = new ServiceCollection();
+
+        // Logging
+        var logger = LoggingConfiguration.CreateLogger();
+        services.AddSingleton<ILogger>(logger);
+
+        // Infrastructure
+        services.AddSingleton<ICommandRunner, CommandRunner>();
+
+        // Services
+        services.AddSingleton<IWslService, WslService>();
+        services.AddSingleton<IDistroService, DistroService>();
+        services.AddSingleton<IDockerService, DockerService>();
+        services.AddSingleton<IDiskService, DiskService>();
+        services.AddSingleton<IDialogService, DialogService>();
+
+        // ViewModels
+        services.AddTransient<MainWindowViewModel>();
+
+        return services.BuildServiceProvider();
     }
 }
